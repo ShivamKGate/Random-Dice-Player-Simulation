2d2572ab515ac14decdcbf401cace7db2ee970c9
Shivam Sinay Kharangate <143129466+ShivamKGate@users.noreply.github.com>
2023-09-04 19:35:59 -0400

    Add files via upload

---
+++ RDPS.m
@@ -0,0 +1,68 @@
+% HOMEWORK 2: TASK 1
+% File: HW5p2_Task1_sinayksp.m
+% Date: 21 February 2023
+% By: SHIVAM KHARANGATE (sinayksp)
+%
+% Section: 019
+% Team: 279
+%
+% ELECTRONIC SIGNATURE
+% Shivam Kharangate
+%
+% The electronic signature above indicates the script
+% submitted for evaluation is my individual work, and I
+% have a general understanding of all aspects of its
+% development and execution.
+%
+% A BRIEF DESCRIPTION OF WHAT THE SCRIPT OR FUNCTION DOES
+%
+
+P1 = input("Enter Player 1 name: ", "s");
+P2 = input("Enter Player 2 name: ", "s");
+n = input("The number of attempts n = ");
+Score1 = 0;
+Score2 = 0;
+P1_dice = 0;
+P2_dice = 0;
+Driver_name ="";
+Follower_name ="";
+for round = 1:(n+1)
+    if round == 1
+        while true
+            P1_dice = randi([1, 6], 1);
+            P2_dice = randi([1, 6], 1);
+            if P1_dice ~= P2_dice
+                break;
+            end
+        end 
+        if P1_dice > P2_dice
+            driver = P1_dice;
+            Driver_name = P1;
+            Follower_name = P2;
+        else
+            driver = P2_dice;
+            Driver_name = P2;
+            Follower_name = P1;
+        end
+        fprintf('The Driver is %s, the Follower is %s \n', Driver_name, Follower_name);
+    else
+        follower_roll = randi([1,6],1);
+        driver_roll = randi([1,6],1);
+        if driver_roll == follower_roll
+            Score2 = Score2 + 1; 
+            fprintf('The winner of round %i is %s \n',(round-1),Follower_name);
+        else
+            Score1 = Score1 + 1; 
+            fprintf('The winner of round %i is %s \n',(round-1),Driver_name);
+        end
+    end
+end
+if Score1 == Score2
+    fprintf('The game is a tie!\n');
+elseif Score1 > Score2
+    fprintf('The winner of the game is %s \n', string(Driver_name));
+else
+    fprintf('The winner of the game is %s \n', string(Follower_name));
+end
+fprintf('The points for %s is %d.\n', Follower_name,Score2);
+fprintf('The points for %s is %d.\n', Driver_name,Score1);
